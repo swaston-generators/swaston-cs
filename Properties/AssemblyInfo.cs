b7a46296da2532f28689cc741bc1544d1921a0df
10 корень из 5 <gd22.3606@gmail.com>
2019-10-31 19:37:43 +0200

    ,

--- Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-// Управление общими сведениями о сборке осуществляется с помощью 
-// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
-// связанные со сборкой.
-[assembly: AssemblyTitle("SwastonGenerator")]
-[assembly: ComVisible(false)]
